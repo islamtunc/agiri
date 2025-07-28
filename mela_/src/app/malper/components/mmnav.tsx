3034763a925dc57b21403c3d2d14d0b44e8b7c72
Yekazad Software Center <91857222+islamtunc@users.noreply.github.com>
2025-07-28 23:11:17 +0300

    Update mmnav.tsx

--- mela_/src/app/malper/components/mmnav.tsx
+++ mela_/src/app/malper/components/mmnav.tsx
@@ -1,4 +1,8 @@
+
 // Bismillahirrahmanirrahim
+// Elhamdülillahirabbulalemin 
+// Esselatu vesselamu ala rasulina Muhammed'in ve ala alihi ve sahbihi ecmain 
+//La ilahe illallah 
 
 "use client"
 
@@ -7,32 +11,30 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Mmmnavbar() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/malper">BARIŞ SÜRÜCÜ KURSU</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/malper">Anasayfa</Nav.Link>
-            <Nav.Link href="/malper/mmavahi"></Nav.Link>
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/malper">BARIŞ SÜRÜCÜ KURSU</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/malper">Anasayfa</Nav.Link>
+            <Nav.Link href="/malper/mmavahi">Ehliyet Sınıfları</Nav.Link>
 
-            <Nav.Link href="/malper/mmkargeh">Hastalıklar ve Beslenme</Nav.Link>
+            <Nav.Link href="/malper/mmkargeh">Direksiyon Eğitimi</Nav.Link>
 
-            <Nav.Link href="/malper/mmhewcedari">Çocuk ve Ergenlerde Beslenme</Nav.Link>
+            <Nav.Link href="/malper/mmhewcedari">Sınav Hazırlığı</Nav.Link>
 
-            <Nav.Link href="/malper/mmwesayit">Diyet Çeşitleri </Nav.Link>
+            <Nav.Link href="/malper/mmwesayit">Kayıt ve Evraklar</Nav.Link>
 
-<Nav.Link href="/malper/mmkedkar">Sağlıklı Tarifler</Nav.Link>
+            <Nav.Link href="/malper/mmkedkar">Sık Sorulan Sorular</Nav.Link>
 
-<Nav.Link href="/malper/mmkinc">Psikoloji ve Beslenme</Nav.Link>
-
-
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+            <Nav.Link href="/malper/mmkinc">İletişim</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Mmmnavbar;
